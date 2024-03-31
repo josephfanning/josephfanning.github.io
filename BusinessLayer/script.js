3c86f4d8d0e4b1a554c0b2bb9e9b02cc7aba42b5
josephfanning <jf338664@gmail.com>
2024-03-31 19:58:43 +0100

    trying to test out API usage for teamTracker.html externally (not natively (localhost) )

--- BusinessLayer/script.js
+++ BusinessLayer/script.js
@@ -140,3 +140,20 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // code for the celticQuiz.html ends here
+
+const apiUrl = "www.thesportsdb.com/api/v1/json/3/search_all_leagues.php?c=Scotland&s=Soccer";
+
+fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log("country info:", countries);
+        // Use the fetched data here
+    })
+    .catch(error => {
+        console.error("There was a problem with the fetch operation:", error);
+    });
